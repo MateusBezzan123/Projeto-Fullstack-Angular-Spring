7705ab6d5e070191455c8214c700df830df298d2
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-10-22 22:07:26 -0300

    Migration V01 funcionando

--- backend/algamoney-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ backend/algamoney-api/src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,10 +1,6 @@
-CREATE TABLE categoria (
-	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categoria (nome) values ('Lazer');
-INSERT INTO categoria (nome) values ('Alimentação');
-INSERT INTO categoria (nome) values ('Supermercado');
-INSERT INTO categoria (nome) values ('Farmácia');
-INSERT INTO categoria (nome) values ('Outros');
+CREATE TABLE public.category (
+                id BIGINT NOT NULL,
+                name VARCHAR(150) NOT NULL,
+                CONSTRAINT id_category PRIMARY KEY (id)
+);
