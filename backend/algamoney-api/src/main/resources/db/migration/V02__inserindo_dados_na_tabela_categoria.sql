965f32a1f0e59ac83ef5ac4b219882cb1ace3666
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-10-23 11:46:15 -0300

    Foi Feito uma migration para inserir dados no Banco de Dados

---
+++ backend/algamoney-api/src/main/resources/db/migration/V02__inserindo_dados_na_tabela_categoria.sql
@@ -0,0 +1,5 @@
+INSERT INTO category (id,name) VALUES (1,'Lazer');
+INSERT INTO category (id,name) VALUES (2,'Alimentação');
+INSERT INTO category (id,name) VALUES (3,'Supermercado');
+INSERT INTO category (id,name) VALUES (4,'Farmácia');
+INSERT INTO category (id,name) VALUES (5,'Outros');
