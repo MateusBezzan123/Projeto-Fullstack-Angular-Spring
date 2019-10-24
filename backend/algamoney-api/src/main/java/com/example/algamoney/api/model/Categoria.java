79e2e7552c7c4f3aaaa7ee407e80fe07169aca4c
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-10-24 13:48:06 -0300

    O listar pronto. E retornando 204

---
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/model/Categoria.java
@@ -0,0 +1,30 @@
+package com.example.algamoney.api.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category")
+public class Categoria {
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+}
