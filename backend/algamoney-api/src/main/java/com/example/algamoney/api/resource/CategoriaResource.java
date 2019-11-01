a6a4f9129493df38cc9aa0a9494ae1e548254c66
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-01 09:06:31 -0300

    Metodo salvar pronto

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,14 +35,18 @@ public class CategoriaResource {
 	}
 	
 	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public void  criar(@RequestBody Categoria categoria, HttpServletResponse response) {
+	public ResponseEntity<Categoria>  criar(@RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
 		.buildAndExpand(categoriaSalva.getCodigo()).toUri();
 		response.setHeader("Location", uri.toASCIIString());
 		
+		return ResponseEntity.created(uri).body(categoriaSalva);
 	}
 	
+	@GetMapping("/{codigo}")
+	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
+		return categoriaRepository.findOne(codigo);
+	}
 
 }
