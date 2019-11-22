4ced0818356e6aa16da7d07ee5c5e089f317c167
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-22 11:10:50 -0300

    Usando eventos para adicionar header Location

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/event/listener/RescursoCriadoListener.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/event/listener/RescursoCriadoListener.java
@@ -1,7 +1,12 @@
 package com.example.algamoney.api.event.listener;
 
+import java.net.URI;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 
@@ -10,8 +15,16 @@ public class RescursoCriadoListener implements ApplicationListener<RecursoCriado
 
 	@Override
 	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		HttpServletResponse response = recursoCriadoEvent.getResponse();
+		Long codigo = recursoCriadoEvent.getCodigo();
+		adicionarHeaderLocation(response, codigo);
 		
-		
+	}
+
+	private void adicionarHeaderLocation(HttpServletResponse response, Long codigo) {
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
+				.buildAndExpand(codigo).toUri();
+				response.setHeader("Location", uri.toASCIIString());
 	}
 	
 	
--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,12 +1,12 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
-import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 
