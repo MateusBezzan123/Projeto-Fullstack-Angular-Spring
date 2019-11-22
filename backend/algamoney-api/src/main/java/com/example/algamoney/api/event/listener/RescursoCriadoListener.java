e08ba099167dc3d80fbb63dea61bf1fd50c56d0c
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-22 10:58:50 -0300

    Event

---
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/event/listener/RescursoCriadoListener.java
@@ -0,0 +1,19 @@
+package com.example.algamoney.api.event.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import com.example.algamoney.api.event.RecursoCriadoEvent;
+
+@Component
+public class RescursoCriadoListener implements ApplicationListener<RecursoCriadoEvent> {
+
+	@Override
+	public void onApplicationEvent(RecursoCriadoEvent recursoCriadoEvent) {
+		
+		
+	}
+	
+	
+
+}
