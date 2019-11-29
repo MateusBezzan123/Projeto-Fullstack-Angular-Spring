04663352000c780406fe86d069ec6641a3fa0173
mateusbhn998@gmail.com <mateusbhn998@gmail.com>
2019-11-29 10:05:28 -0300

    Endereço atualizado e Categoria também

--- backend/algamoney-api/src/main/java/com/example/algamoney/api/model/Endereco.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/model/Endereco.java
@@ -1,81 +1,70 @@
 package com.example.algamoney.api.model;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
-@Entity
-@Table(name = "endereco")
 @Embeddable
 public class Endereco {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@NotNull
-	@Size(min = 3, max = 50)
+
 	private String logradouro;
-	@NotNull
-	@Size(min = 3, max = 50)
-	private float numero;
-	@NotNull
-	@Size(min = 3, max = 50)
+	private String numero;
 	private String complemento;
-	@NotNull
-	@Size(min = 3, max = 50)
 	private String bairro;
-	@NotNull
-	@Size(min = 3, max = 50)
-	private float cep;
-	@NotNull
-	@Size(min = 3, max = 50)
+	private String cep;
 	private String cidade;
-	@NotNull
-	@Size(min = 3, max = 50)
 	private String estado;
+	
 	public String getLogradouro() {
 		return logradouro;
 	}
+
 	public void setLogradouro(String logradouro) {
 		this.logradouro = logradouro;
 	}
-	public float getNumero() {
+
+	public String getNumero() {
 		return numero;
 	}
-	public void setNumero(float numero) {
+
+	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public String getComplemento() {
 		return complemento;
 	}
+
 	public void setComplemento(String complemento) {
 		this.complemento = complemento;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-	public float getCep() {
+
+	public String getCep() {
 		return cep;
 	}
-	public void setCep(float cep) {
+
+	public void setCep(String cep) {
 		this.cep = cep;
 	}
+
 	public String getCidade() {
 		return cidade;
 	}
+
 	public void setCidade(String cidade) {
 		this.cidade = cidade;
 	}
+
 	public String getEstado() {
 		return estado;
 	}
+
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
--- backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
+++ backend/algamoney-api/src/main/java/com/example/algamoney/api/resource/CategoriaResource.java
@@ -1,6 +1,6 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
+
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
 import com.example.algamoney.api.model.Categoria;
@@ -33,10 +33,8 @@ public class CategoriaResource {
 	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
-	public ResponseEntity<?> listar(){
-		List<Categoria> categorias = categoriaRepository.findAll();
-		return !categorias.isEmpty() ? ResponseEntity.ok(categorias) : 
-			ResponseEntity.noContent().build();
+	public List<Categoria> listar(){
+		return categoriaRepository.findAll();
 		
 	}
 	
@@ -48,8 +46,9 @@ public class CategoriaResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);	}
 	
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findOne(codigo);
+	public ResponseEntity <Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		 Categoria categoria = categoriaRepository.findOne(codigo);
+		 return categoria != null ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
 	}
 
 }
